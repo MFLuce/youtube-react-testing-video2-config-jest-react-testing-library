89a1725e20e248f53643035a1cd0d01060f73955
MFLuce <mfluce@yahoo.fr>
2022-04-06 11:08:22 +0200

    after linting is configured

--- src/components/Hello.spec.tsx
+++ src/components/Hello.spec.tsx
@@ -1,5 +1,6 @@
 import {render, screen} from '@testing-library/react';
 import { Hello } from './Hello';
+
 it('renders "Hello World"', () => {
 render(<Hello/>);
 const myElement = screen.getByText('Hello World!');
